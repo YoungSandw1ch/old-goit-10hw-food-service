2beb15779248c1c97a6c7fccd1b4ba47ffdad2e1
Sergei Mikhailenko <sersus1991@gmail.com>
2022-10-04 17:28:59 +0300

    add template and css for pokemon api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import menu from './menu.json';
-// import menuItemsTemplates from './templates/menu-items.hbs';
-import menuItemsTemplates from './templates/menu-items.hbs';
-console.log(menuItemsTemplates);
-
-const menuItemsMarkup = menuItemsTemplates(menu);
-const menuListRef = document.querySelector('.js-menu');
-
-menuListRef.insertAdjacentHTML('beforeend', menuItemsMarkup);
+import './css/common.css';
