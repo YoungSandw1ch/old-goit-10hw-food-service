89b4b7597c3483e40919d4e41c634f6ee6cc59a5
Sergei Mikhailenko <sersus1991@gmail.com>
2022-10-08 22:51:52 +0300

    start create new searcher api servise

---
+++ src/js/02-news-api.js
@@ -0,0 +1,31 @@
+const BASE_URL = 'https://newsapi.org/v2';
+const API_KEY = 'bc964c2173954576ae1b4bb3a5d64612';
+const options = {
+  headers: {
+    'X-Api-Key': API_KEY,
+  },
+};
+
+export default class FetchNewsService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchNews() {
+    return fetch(
+      `${BASE_URL}/everything?q=${this.searchQuery}&page=${this.page}&pageSize=5`,
+      options,
+    )
+      .then(response => response.json())
+      .then(({ articles }) => articles);
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/js/02-news-searcher.js
+++ src/js/02-news-searcher.js
@@ -1,1 +1,39 @@
+import FetchNewsService from './02-news-api';
+import articlesTpl from '../templates/news-articles.hbs';
 
+const refs = {
+  form: document.querySelector('.js-search-form'),
+  submitBtn: document.querySelector('.js-submit'),
+  articles: document.querySelector('.js-articles-container'),
+  loadMoreBtn: document.querySelector('.button'),
+};
+
+const newsFetcher = new FetchNewsService();
+console.log(newsFetcher);
+
+refs.form.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  if (!searchQuery) {
+    alert('введите что то в поиск');
+    return;
+  }
+
+  newsFetcher.query = form.elements.query.value;
+
+  newsFetcher.fetchNews().then(renderNews).finally(form.reset());
+}
+
+function onLoadMore() {
+  console.log('load more');
+}
+
+function renderNews(articles) {
+  console.log(articles);
+  const articlesMarkup = articlesTpl(articles);
+  refs.articles.innerHTML = articlesMarkup;
+}
